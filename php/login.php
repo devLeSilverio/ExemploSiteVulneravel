f66b33d686bac8fab431acf66ec7235b55ba9c8c
João Victor da Silva <jv782063@gmail.com>
2021-12-06 23:02:58 -0300

    faz a validação da entrada dos inputs de login e senha caso algum script sql ou xss seja colocado

--- php/login.php
+++ php/login.php
@@ -1,16 +1,18 @@
 <?php
-    require("../banco/conecta.php");
-    session_start();
-    $senha = $_POST['senha'];
-    $login = $_POST['usuario'];
-    
-    $query = "SELECT * FROM tb_admin WHERE nm_admin ='$login' AND ds_senha = '$senha'";
-    $result = mysqli_query($conexao, $query);
-    echo $query;
-    if(mysqli_num_rows($result)==1){
-        $_SESSION['acesso'] = "admin";
-        header('location:../inicio.php');
-    }else{
-         header('location:../index.php');
-    }
-    
+require("../banco/conecta.php");
+session_start();
+$senha = $_POST['senha'];
+$login = $_POST['usuario'];
+
+$login = preg_replace('/[^[:alnum:]_.-]/', '', $login);
+$senha = addslashes($senha);
+
+$query = "SELECT * FROM tb_admin WHERE nm_admin ='$login' AND ds_senha = '$senha'";
+$result = mysqli_query($conexao, $query);
+echo $query;
+if (mysqli_num_rows($result) == 1) {
+    $_SESSION['acesso'] = "admin";
+    header('location:../inicio.php');
+} else {
+    header('location:../index.php');
+}
